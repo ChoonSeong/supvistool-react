f09cc0332df3aec9df7a1629f9d25831c0891afe
Seongyapp <xiang8d@gmail.com>
2024-10-08 03:56:19 +0800

    Change dashboard import path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // Import React
-import React from 'react';
+import React from "react";
 // Import the Dashboard component
-import Dashboard from './Dashboard'; // Ensure the path is correct
+import Dashboard from "./pages/Dashboard"; // Ensure the path is correct
 // Import the CSS file for styling
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
